1b453f21713aa81ed43f6b3f33d077b524601994
kbjude <kbjude@gmail.com>
2020-07-06 22:54:26 +0300

    Group associatins

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   validates :description, presence: true, length: { maximum: 40 }
+
+  has_many :grouphours, dependent: :destroy
+  has_many :hours, through: :grouphours, source: :hour
 end
