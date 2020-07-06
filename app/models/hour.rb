abf3ea58544472658cd7a02da4e1f2875eac0ddf
kbjude <kbjude@gmail.com>
2020-07-06 22:49:35 +0300

    Associations

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -1,3 +1,5 @@
 class Hour < ApplicationRecord
+    has_many :grouphours , dependent: :destroy
+    has_many :groups, through: :grouphours, source :group
   validates :hours, prescence: true, numericality: { only_integer: true }
 end
