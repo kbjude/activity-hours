091b5ba0f6ad408dda4616fbf35c6be4ad69c611
kbjude <kbjude@gmail.com>
2020-07-07 03:10:41 +0300

    Routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get '/signup', to: 'users#new'
+  get '/logout', to: 'sessions#destroy'
+  get '/signin', to: 'sessions#new'
 end
