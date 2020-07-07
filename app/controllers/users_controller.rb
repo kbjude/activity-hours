031236a3acf77660b2ddd7b574fe8c4e7cd4de3e
kbjude <kbjude@gmail.com>
2020-07-07 04:11:43 +0300

    Adding other controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,22 @@ class UsersController < ApplicationController
     end
 
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.create(params)
     end
 
     def show
+        @user = User.find_by_id([:user_id])
     end
 
-    def destroy
-    end
+    # def destroy
+    # end
 
-    def update
-    end
+    # def update
+    # end
 
     private
     def user_params
