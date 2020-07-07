14fb71bafcc13752779762443012f92cfd05aa53
kbjude <kbjude@gmail.com>
2020-07-07 22:05:26 +0300

    routes and deleting of commented code in the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  resources :groups
+  resources :hours
   resources :sessions, only: %i[new create destroy]
   get '/signup', to: 'users#new'
   get '/logout', to: 'sessions#destroy'
