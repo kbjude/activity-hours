04c43fd32402cc919c00073111277cb4c1a2868d
kbjude <kbjude@gmail.com>
2020-07-07 03:57:01 +0300

    Add Groups and user relationshid

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   validates :description, presence: true, length: { maximum: 40 }
 
+  belongs_to :user, dependent: :destroy
   has_many :grouphours, dependent: :destroy
   has_many :hours, through: :grouphours, source: :hour
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_706_173_120) do
-  create_table 'grouphours', force: :cascade do |t|
-    t.integer 'hour_id'
-    t.integer 'group_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['group_id'], name: 'index_grouphours_on_group_id'
-    t.index ['hour_id'], name: 'index_grouphours_on_hour_id'
+ActiveRecord::Schema.define(version: 2020_07_07_005329) do
+
+  create_table "grouphours", force: :cascade do |t|
+    t.integer "hour_id"
+    t.integer "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_grouphours_on_group_id"
+    t.index ["hour_id"], name: "index_grouphours_on_hour_id"
   end
 
-  create_table 'groups', force: :cascade do |t|
-    t.string 'name'
-    t.string 'icon'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  create_table 'hours', force: :cascade do |t|
-    t.integer 'hours'
-    t.text 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.index ['user_id'], name: 'index_hours_on_user_id'
+  create_table "hours", force: :cascade do |t|
+    t.integer "hours"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_hours_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
