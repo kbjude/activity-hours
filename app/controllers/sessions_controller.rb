1c532860030640619b2379a734da9e687dec6da4
kbjude <kbjude@gmail.com>
2020-07-07 03:37:46 +0300

    login and part of show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,34 @@
 class SessionsController < ApplicationController
-    skip_before_action :login_required, only: %i[new create]
-    def new
-        @user = User.new
-    end
+  skip_before_action :login_required, only: %i[new create]
+  def new
+    @user = User.new
+  end
 
-    def create
-        if @user = User.find_by_name(params[:name])
-            session[:user_id] = @user.user_id
-            redirect_to root_path
-        elsif flash[:alert] = 'Invalid name'
-            @user = User.new(params)
-            redirect_to root_path
-        else
-            render 'new'
-        end
+  def create
+    @user = User.find_by_name(params[:name])
+    if @user
+      session[:user_id] = @user.user_id
+      redirect_to root_path
+    elsif flash[:alert] == 'Invalid name'
+      @user = User.new(params)
+      redirect_to root_path
+    else
+      render 'new'
     end
+  end
 
-    def destroy
-        session[user_id] = nill
-        redirect_to root_path
-    end
+  def destroy
+    session[user_id] = nil
+    redirect_to root_path
+  end
 
-    def show
-        
-    end
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+
+  def login(_user)
+    session[:user_id] = nil
+  end
 end
