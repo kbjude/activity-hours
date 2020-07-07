ff95cf04e600af831cc1670958991cb9e460829c
kbjude <kbjude@gmail.com>
2020-07-07 22:33:17 +0300

    WIP groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,30 @@
 class GroupsController < ApplicationController
+    def index
+        @groups = Group.all
+      end
+    
+      def new
+        @group = Group.new
+      end
+    
+      def create
+        @group = Group.new(hour_params)
+        @group.save
+        if @group.save
+          flash.now[:succes] = 'Group successful created'
+          redirect_to @group
+        else
+          render :new
+        end
+      end
+    
+      def show
+        @group = Group.find_by_id([:group_id])
+      end
+    
+      private
+    
+      def group_params
+        params.require(:group).permit(:name, :icon, :user_id)
+      end
 end
