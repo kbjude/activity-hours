65be70d963ba09b8f9656aec7f79614e515dc294
kbjude <kbjude@gmail.com>
2020-07-09 17:15:33 +0300

    Trailing Space

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :groups
-  resources :hours do 
+  resources :hours do
     collection do
       get 'externalhours'
     end
