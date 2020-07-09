43233657030ef5a57cf239c733686a028aa7d6d0
kbjude <kbjude@gmail.com>
2020-07-09 17:01:29 +0300

    Adding edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :groups
+  resources :hours do 
+    collection do
+      get 'externalhours'
+    end
+  end
+  resources :sessions, only: %i[new create destroy]
+  get '/signup', to: 'users#new'
+  get '/logout', to: 'sessions#destroy'
+  get '/signin', to: 'sessions#new'
+  root 'users#show'
 end
