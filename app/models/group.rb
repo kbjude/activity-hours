15dd419b5dfd030945a2da2d38b0a3dabc541f28
kbjude <kbjude@gmail.com>
2020-07-11 13:08:57 +0300

    Cleaning comments in the group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,16 +6,3 @@ class Group < ApplicationRecord
   has_many :hours, through: :grouphours, source: :hour
   has_one_attached :icon
 end
-
-# def thumbnail
-  # return self.icon.variant(resize: '200x200').processed
-# end
-
-def resize_image
-  resized_image = MiniMagick::Image.read(icon.download)
-  resized_image = resized_image.resize "400x400"
-  v_filename = obj.filename
-  v_content_type = obj.content_type
-  obj.purge
-  obj.attach(io: File.open(resized_image.path), filename:  v_filename, content_type: v_content_type)
-end
