d0c5138c2454d0b3119b643ee99b741a7ce0704d
kbjude <kbjude@gmail.com>
2020-07-11 14:11:58 +0300

    Adding only: required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :groups
+  resources :users, only: %i[new create show index]
+  resources :groups, only: %i[new create show index]
   resources :hours do
     collection do
       get 'externalhours'
