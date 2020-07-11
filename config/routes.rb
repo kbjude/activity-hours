5d7158eb740e5281ef619df9ece1e80840ec34f5
kbjude <kbjude@gmail.com>
2020-07-11 14:18:21 +0300

    Improving the hours route to the used routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create show index]
   resources :groups, only: %i[new create show index]
-  resources :hours do
+  resources :hours, only: %i[new create show index] do
     collection do
       get 'externalhours'
     end
