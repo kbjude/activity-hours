59e2db0a1e4b5b8cedd8fd78e60815e8b83d8a96
kbjude <kbjude@gmail.com>
2020-07-15 02:14:47 +0300

    Testing lists

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'User', type: :model do
             user = User.create(name: 'Jude')
             expect(user).to eql(user)
         end
-        it 'returns a verified user' do
+        it 'returns a verified user at login' do
             user = User.create(name: '')
             expect(user.valid?).to eql(false)
         end
