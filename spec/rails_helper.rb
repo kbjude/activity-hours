afe289f9dd56b959618da81db1d125b515858874
kbjude <kbjude@gmail.com>
2020-07-15 01:40:02 +0300

    Working first spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,5 +15,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include Devise::Test::ControllerHelpers, type: :controller
 end
