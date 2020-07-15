8a061aa8a56cc7523f290cc60a4d1ba7d35c6a10
kbjude <kbjude@gmail.com>
2020-07-15 10:23:12 +0300

    Adding feature tests

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,17 +1,10 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :feature do
-#     context 'Login attemps' do
-#       it 'returns login form when no user is logged in' do
-#         visit new_user_session_path
-#         expect(page).to have_content('Sign in')
-#       end
-#       it 'User is redirected to the index page' do
-#         visit 'users/new'
-#         fill_in 'user_email', with: 'user.email'
-#         fill_in 'user_password', with: 'user.jude'
-#         click_button 'Log in'
-#         expect(page).to have_content('Stay in touch')
-#       end
-#     end
-#   end
+RSpec.describe User, type: :feature do
+    context 'Login attemps' do
+      it 'returns login form when no user is logged in' do
+        visit new_session_path
+        expect(page).to have_content('Login')
+      end
+    end
+  end
