4542bea904c643d0a1f9e43f58c854819ac5ef97
kbjude <kbjude@gmail.com>
2020-07-20 03:42:11 +0300

    Removing logic from views WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def header
+        if logged_in?
+            # concat You are signed as, "#{current_user.name}"
+            concat link_to 'Logout', logout_path
+            concat link_to 'Back to Profile', root_path
+            concat link_to 'Add a new hour(s)', new_hour_path
+            else
+            concat link_to 'Login', signin_path
+            concat link_to 'Sign Up', new_user_path
+        end
+    end
 end
