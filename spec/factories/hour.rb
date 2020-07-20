186ab8443a946ead130c48f621ef4cb9b960efd1
kbjude <kbjude@gmail.com>
2020-07-21 00:36:04 +0300

    Adding specs for hours

--- spec/factories/hour.rb
+++ spec/factories/hour.rb
@@ -1,11 +1,11 @@
-# FactoryBot.define do
-#     factory :hour, class: Hour do
-#         hours { Faker::Hour.hours }
-#         description { Faker::Description.description } 
-#     end
+FactoryBot.define do
+    factory :hour, class: Hour do
+        hours       { 6 }
+        description { "Ride" }
+    end
 
-#     factory :random_hours, class: Hour do
-#         hours { Faker::Hour.hours }
-#         description { Faker::Description.description }  
-#     end
-# end
+    factory :random_hours, class: Hour do
+        hours { Faker::Hours.hours }
+        description { Faker::Hours.description }
+    end
+end
--- spec/model/hour_spec.rb
+++ spec/model/hour_spec.rb
@@ -1,13 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Hour, type: :model do
-#     describe 'Creating an hour' do
-#         let(:hours) { build(:hour)}
-#         let(:user) { create(:user)}
-#         it 'returns a newly created user hour' do
-#             hours.user_id = user.id
-#             expect(hours.save).to eql(true)
-#         end
+RSpec.describe Hour, type: :model do
+    describe 'Creating an hour' do
+        let(:hour) { build(:hour)}
+        let(:user) { create(:user)}
+        it 'returns a newly created user hour' do
+            hour.user_id = user.id
+            expect(hour.save).to eql(true)
+        end
 
-#     end
-# end
+    end
+end
