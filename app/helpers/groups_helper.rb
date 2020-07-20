d75e32c2343111f61d2bc1fae70754f0b311701e
kbjude <kbjude@gmail.com>
2020-07-20 04:38:21 +0300

    Sorting up the groups feature

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,7 +1,21 @@
 module GroupsHelper
     def attached
-        if group.icon.attached?
-            concat image_tag(group.icon.variant(resize: '200 x 200'))
+        @groups.each do |group|
+            if group.icon.attached?
+                concat image_tag(group.icon.variant(resize: '200 x 200'))
+            end
+        end
+    end
+
+    def uploads
+        @group.uploads.each do |upload|
+            if upload.variable?
+                concat image_tag(upload.variant(resize: '400 x 400'))
+            elsif upload.previewable?
+                concat link_to image_tag(resize.uploads.preview('400 x 400'), upload)
+            elsif upload.image?
+                concat link_to image_tag(resize.uploads.preview('400 x 400'), upload)
+            end
         end
     end
 end
