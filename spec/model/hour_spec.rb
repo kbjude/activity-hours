dce57119b50a54de9215ab633b2237df5511454a
kbjude <kbjude@gmail.com>
2020-07-20 23:34:49 +0300

    Adding the features tests

--- spec/model/hour_spec.rb
+++ spec/model/hour_spec.rb
@@ -1,15 +1,13 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Hour', type: :model do
-    describe 'Creating a hour' do
-        it 'returns a newly created user hour' do
-            user = User.create(name: 'Jude')
-            hour = Hour.create(hours: 2, description: "Praying", user_id: 1)
-            expect(Hour.first.description).to eql("Praying")
-        end
-        it 'returns all the registered hours' do
-            hour = Hour.all
-            expect(hour).to eql(hour)
-        end
-    end
-end
+# RSpec.describe Hour, type: :model do
+#     describe 'Creating an hour' do
+#         let(:hours) { build(:hour)}
+#         let(:user) { create(:user)}
+#         it 'returns a newly created user hour' do
+#             hours.user_id = user.id
+#             expect(hours.save).to eql(true)
+#         end
+
+#     end
+# end
