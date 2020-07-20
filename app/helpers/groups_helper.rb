35632229c18858d16f4000616a7c520394580e7c
kbjude <kbjude@gmail.com>
2020-07-20 04:40:42 +0300

    Fixes

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,21 +1,19 @@
 module GroupsHelper
-    def attached
-        @groups.each do |group|
-            if group.icon.attached?
-                concat image_tag(group.icon.variant(resize: '200 x 200'))
-            end
-        end
+  def attached
+    @groups.each do |group|
+      concat image_tag(group.icon.variant(resize: '200 x 200')) if group.icon.attached?
     end
+  end
 
-    def uploads
-        @group.uploads.each do |upload|
-            if upload.variable?
-                concat image_tag(upload.variant(resize: '400 x 400'))
-            elsif upload.previewable?
-                concat link_to image_tag(resize.uploads.preview('400 x 400'), upload)
-            elsif upload.image?
-                concat link_to image_tag(resize.uploads.preview('400 x 400'), upload)
-            end
-        end
+  def uploads
+    @group.uploads.each do |upload|
+      if upload.variable?
+        concat image_tag(upload.variant(resize: '400 x 400'))
+      elsif upload.previewable?
+        concat link_to image_tag(resize.uploads.preview('400 x 400'), upload)
+      elsif upload.image?
+        concat link_to image_tag(resize.uploads.preview('400 x 400'), upload)
+      end
     end
+  end
 end
